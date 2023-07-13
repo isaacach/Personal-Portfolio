df0eebba37e01f916897e45f143d89aca87aecb3
isaacach <124728947+isaacach@users.noreply.github.com>
2023-07-12 20:19:20 -0500

    added more styles to load animation

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 let overlay = document.querySelector('.overlay');
+let container = document.querySelector('.container');
 
 setTimeout(() => {
     overlay.style.display = 'none';
 }
 , 5400);
+
+setTimeout(() => {
+  container.style.display = 'flex';
+}
+, 4000);
