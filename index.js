aa7216fce9c37be5956ded1c6d554253890123a9
isaacach <124728947+isaacach@users.noreply.github.com>
2023-07-14 22:23:03 -0500

    added email service

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let overlay = document.querySelector('.overlay');
 let container = document.querySelector('.container');
-
 let home = document.querySelector('.home');
+let toTop = document.querySelector('.footer-icon-wrapper');
 
 home.addEventListener('click', () => {
   scrollTo({
@@ -11,6 +11,12 @@ home.addEventListener('click', () => {
   });
 });
 
-
+toTop.addEventListener('click', () => {
+  scrollTo({
+    top: 0, 
+    left: 0, 
+    behavior: "smooth"
+  });
+});
 
 
